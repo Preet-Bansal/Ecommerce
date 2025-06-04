5d5b29c0a9ba84696571450e0b9df61f82d7845d
Preet-Bansal <preetbansal8902@gmail.com>
2025-06-04 16:58:34 +0530

    Added ResponseStatus

--- src/main/java/com/example/Ecom/controller/ProductController.java
+++ src/main/java/com/example/Ecom/controller/ProductController.java
@@ -4,6 +4,8 @@ import com.example.Ecom.model.Product;
 import com.example.Ecom.service.ProductService;
 import lombok.experimental.PackagePrivate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,14 +26,18 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public List<Product> getAllProducts(){
-        return service.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
     }
 
     @GetMapping("/products/{id}")
-    public Product getProduct(@PathVariable int id){
+    public ResponseEntity<Product>  getProduct(@PathVariable int id){
 
-        return service.getProductById(id);
+        Product product = service.getProductById(id);
+        if(product != null)
+        return new ResponseEntity<> (product, HttpStatus.FOUND);
+        else
+            return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 
